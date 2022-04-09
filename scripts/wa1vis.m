6b06ec2c85d4341202d12470f7f3d2916df91e54
Eris Lund <38136789+0x5066@users.noreply.github.com>
2022-04-09 04:07:12 +0200

    now the vis actually greets you on skin load

--- scripts/wa1vis.m
+++ scripts/wa1vis.m
@@ -4,7 +4,7 @@
 #include "..\..\..\lib/std.mi"
 
 Global AnimatedLayer band1, band2, band3, band4, band5, band6, band7, band8, band9, band10, band11, band12;
-//Global text banddebug;
+Global text banddebug;
 Global Timer wa100analyzers;
 Global float an1, an2, an3, an4, an5, an6, an7, an8, an9, an10, an11, an12;
 Global float clip1, clip2, clip3, clip4, clip5, clip6, clip7, clip8, clip9, clip10, clip11, clip12;
@@ -12,6 +12,7 @@ Global int band1int, band2int, band3int, band4int, band5int, band6int, band7int,
 
 Function setAmplitude();
 Function anfo();
+Function setLevels();
 
 System.onScriptLoaded(){
 
@@ -30,10 +31,11 @@ System.onScriptLoaded(){
     band11 = visgroup.findObject("band11");
     band12 = visgroup.findObject("band12");
 
-    //banddebug = visgroup.findObject("analyzerdebug");
+    banddebug = visgroup.findObject("analyzerdebug");
 
     wa100analyzers = new Timer;
 	wa100analyzers.setDelay(0);
+
     setAmplitude();
 
 }
@@ -88,23 +90,8 @@ wa100analyzers.onTimer(){
 }
 
 setAmplitude(){
-    anfo();
-
-    float divideby = 4.5;
-    float dc = 340;
-
-    band1int = ((getVisBand(0,0)+getVisBand(0,1)+getVisBand(0,2)+getVisBand(0,3)+getVisBand(0,4)+getVisBand(0,5))/divideby)*6;
-    band2int = ((getVisBand(0,6)+getVisBand(0,7)+getVisBand(0,8)+getVisBand(0,9)+getVisBand(0,10)+getVisBand(0,11))/divideby)*4;
-    band3int = ((getVisBand(0,12)+getVisBand(0,13)+getVisBand(0,14)+getVisBand(0,15)+getVisBand(0,16)+getVisBand(0,17))/divideby)*3.5;
-    band4int = ((getVisBand(0,18)+getVisBand(0,19)+getVisBand(0,20)+getVisBand(0,21)+getVisBand(0,22)+getVisBand(0,23))/divideby)*3;
-    band5int = ((getVisBand(0,24)+getVisBand(0,25)+getVisBand(0,26)+getVisBand(0,27)+getVisBand(0,28)+getVisBand(0,29))/divideby)*2;
-    band6int = ((getVisBand(0,30)+getVisBand(0,31)+getVisBand(0,32)+getVisBand(0,33)+getVisBand(0,34)+getVisBand(0,35))/divideby)*2;
-    band7int = ((getVisBand(0,36)+getVisBand(0,37)+getVisBand(0,38)+getVisBand(0,39)+getVisBand(0,40)+getVisBand(0,41))/divideby)*1.5;
-    band8int = ((getVisBand(0,42)+getVisBand(0,43)+getVisBand(0,44)+getVisBand(0,45)+getVisBand(0,46)+getVisBand(0,47))/divideby)*1.5;
-    band9int = ((getVisBand(0,48)+getVisBand(0,49)+getVisBand(0,50)+getVisBand(0,51)+getVisBand(0,52)+getVisBand(0,53))/divideby);
-    band10int = ((getVisBand(0,54)+getVisBand(0,55)+getVisBand(0,56)+getVisBand(0,57)+getVisBand(0,58)+getVisBand(0,59))/divideby);
-    band11int = ((getVisBand(0,60)+getVisBand(0,61)+getVisBand(0,62)+getVisBand(0,63)+getVisBand(0,64)+getVisBand(0,65))/divideby);
-    band12int = ((getVisBand(0,66)+getVisBand(0,67)+getVisBand(0,68)+getVisBand(0,69)+getVisBand(0,70)+getVisBand(0,71))/divideby);
+    //anfo();
+    setLevels();
 
     band1.gotoFrame(an1*band1.getLength()/256);
     band2.gotoFrame(an2*band1.getLength()/256);
@@ -120,8 +107,23 @@ setAmplitude(){
     band12.gotoFrame(an12*band1.getLength()/256);
 }
 
-anfo(){
+setLevels(){
+    float divideby = 4.5;
+    float dc = 340;
     float falloff = 26;
+
+    band1int = ((getVisBand(0,0)+getVisBand(0,1)+getVisBand(0,2)+getVisBand(0,3)+getVisBand(0,4)+getVisBand(0,5))/divideby)*6;
+    band2int = ((getVisBand(0,6)+getVisBand(0,7)+getVisBand(0,8)+getVisBand(0,9)+getVisBand(0,10)+getVisBand(0,11))/divideby)*4;
+    band3int = ((getVisBand(0,12)+getVisBand(0,13)+getVisBand(0,14)+getVisBand(0,15)+getVisBand(0,16)+getVisBand(0,17))/divideby)*3.5;
+    band4int = ((getVisBand(0,18)+getVisBand(0,19)+getVisBand(0,20)+getVisBand(0,21)+getVisBand(0,22)+getVisBand(0,23))/divideby)*3;
+    band5int = ((getVisBand(0,24)+getVisBand(0,25)+getVisBand(0,26)+getVisBand(0,27)+getVisBand(0,28)+getVisBand(0,29))/divideby)*2;
+    band6int = ((getVisBand(0,30)+getVisBand(0,31)+getVisBand(0,32)+getVisBand(0,33)+getVisBand(0,34)+getVisBand(0,35))/divideby)*2;
+    band7int = ((getVisBand(0,36)+getVisBand(0,37)+getVisBand(0,38)+getVisBand(0,39)+getVisBand(0,40)+getVisBand(0,41))/divideby)*1.5;
+    band8int = ((getVisBand(0,42)+getVisBand(0,43)+getVisBand(0,44)+getVisBand(0,45)+getVisBand(0,46)+getVisBand(0,47))/divideby)*1.5;
+    band9int = ((getVisBand(0,48)+getVisBand(0,49)+getVisBand(0,50)+getVisBand(0,51)+getVisBand(0,52)+getVisBand(0,53))/divideby);
+    band10int = ((getVisBand(0,54)+getVisBand(0,55)+getVisBand(0,56)+getVisBand(0,57)+getVisBand(0,58)+getVisBand(0,59))/divideby);
+    band11int = ((getVisBand(0,60)+getVisBand(0,61)+getVisBand(0,62)+getVisBand(0,63)+getVisBand(0,64)+getVisBand(0,65))/divideby);
+    band12int = ((getVisBand(0,66)+getVisBand(0,67)+getVisBand(0,68)+getVisBand(0,69)+getVisBand(0,70)+getVisBand(0,71))/divideby);
 
     //1,2
     if (band1int >= an1){
